6049b9f43862578fcd5bec2fac6fdf0b50a9cb8a
김태민 <seolin1218@gmail.com>
2022-10-26 15:29:26 +0900

    String -> enum 타입으로 컨버팅되게 수정

--- src/main/java/com/portfolio/gascharge/config/WebConverterConfiguration.java
+++ src/main/java/com/portfolio/gascharge/config/WebConverterConfiguration.java
@@ -4,12 +4,14 @@ import com.portfolio.gascharge.config.converter.StringToAuthProviderConverter;
 import com.portfolio.gascharge.config.converter.StringToChargePlaceMembershipConverter;
 import com.portfolio.gascharge.config.converter.StringToUserAuthorityConverter;
 import com.portfolio.gascharge.config.converter.StringToUserEmailVerifiedConverter;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Configuration
 public class WebConverterConfiguration implements WebMvcConfigurer {
 
     @Override
